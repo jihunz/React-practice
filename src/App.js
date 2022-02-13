631c0f23d2496169c16efa03f09cc9d776c85d9f
Jihunz <thespringsong@gmail.com>
2022-02-13 14:30:45 +0900

    feat: onKeyUp 이벤트 등록

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react';
 function App() {
 
   const onSubmit = () => {
-    alert("submitted");
+    alert("submitted success");
+  };
+
+  const onKeyUp = (event) => {
+    if(event.keyCode === 13){
+      onSubmit();
+    }
   };
 
   return (
     <div className="App">
+      <input onKeyUp={onKeyUp}/>
       <button onClick={onSubmit}>Submit</button>
     </div>
   );
