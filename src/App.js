88bba31eaf3b4ff1e00ce9a96a871742be07a23f
Jihunz <thespringsong@gmail.com>
2022-02-13 16:57:11 +0900

    컴포넌트 실습

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [j, setJ] = useState(0);
 
-  useEffect(() => {
-    console.log(count);
-  }, [count, j])
-
-  useEffect(() => {
-    console.log("first rendering");
-  }, [])
-
-  const increment = () => {
-    setCount(count + 1);
-  }
-
-  return (
-    <div className="App">
-      useEffect
-      <button onClick={increment}>Click</button>
-      <button onClick={() => setJ(j + 1)}>Click2</button>
+  return(
+    <div className='App'>
+      <h1>Jihun</h1>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
-  );
+  )
 }
 
 export default App;
