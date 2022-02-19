99afd752eb3e07a82522a269d0302e1c72666360
Jihunz <thespringsong@gmail.com>
2022-02-19 18:59:07 +0900

    Router , Switch, Route 적용

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Home</a>
+            <Link class="navbar-brand" to="/">Home</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Moives</a>
+                  <Link class="nav-link active" aria-current="page" to="/movies">Moives</Link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">users</a>
+                  <Link class="nav-link" to="/users">users</Link>
                 </li>
               </ul>
               <form class="d-flex">
