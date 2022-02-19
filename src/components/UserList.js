ce8d2a7de5058065133eb42a64973fc16b38bf6f
Jihunz <thespringsong@gmail.com>
2022-02-19 19:40:44 +0900

    로딩 스피너 추가

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const UserList = ({ users }) => {
-    console.log(users);
+
     return (
         <div>
             {users.map(user => {
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserList from "../components/UserList";
+import Spinner from "../components/Spinner";
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
-            setUsers(response.data)
+            setUsers(response.data);
+            setLoading(false);
         });
     }, []);
 
@@ -19,7 +22,7 @@ const Users = () => {
     return (
         <>
             <h1>Users</h1>
-            <UserList users={users}/>
+            {loading ? <Spinner /> : <UserList users={users}/>}
         </>
     )
 }
