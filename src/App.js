4fcd831d39a481d9fa39142fad243f075d0cd7cb
Jihunz <thespringsong@gmail.com>
2022-02-19 18:08:25 +0900

    부트스트랩 navbar 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
+import Navbar from './components/Navbar';
+
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -30,6 +32,7 @@ function App() {
 
   return(
     <div className='App'>
+      <Navbar />
       <h1>Movie list</h1>
       <MovieForm addMovie={addMovie}/>
       {renderMovies}
