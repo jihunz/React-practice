5477c9262eff2c7d56eb404fddc3aa62e6f4800f
Jihunz <thespringsong@gmail.com>
2022-02-19 19:52:38 +0900

    page와 components 모두 별도의 폴더로 각각 분리

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import Movie from './components/Movie';
-import MovieForm from './components/MovieForm';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Users from './pages/Users';
+import Home from './pages/Home';
+import Moives from './pages/Moives';
 import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(movies.filter(movie => {
-      return movie.id !== id;
-    }))
-  };
-
-  const renderMovies = movies.length ? movies.map(movie => {
-    return (
-      <Movie 
-        movie={movie} 
-        key={movie.id}
-        removeMovie={removeMovie}  
-      />
-    );
-  }) : '추가된 영화가 없습니다';
-
-  const addMovie = (movie) => {
-    setMovies([
-      ...movies,
-      movie
-    ]);
-  };
 
   return(
     <Router>
@@ -38,15 +14,13 @@ function App() {
         <div className='container'>
           <Switch>
             <Route path='/movies'>
-              <h1>Movie list</h1>
-              <MovieForm addMovie={addMovie}/>
-              {renderMovies}
+              <Moives />
             </Route>
             <Route path='/users'>
               <Users />
             </Route>
             <Route path='/' exact>
-              <h1>Home</h1>
+              <Home />
             </Route>
           </Switch>
         </div>
